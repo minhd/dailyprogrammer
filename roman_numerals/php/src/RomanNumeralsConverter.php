ae3a4688ad0a09e67cbd48a5bafb1a2e0477b62b
Minh Duc Nguyen <dekarvn@gmail.com>
2016-06-26 20:57:53 +1000

    Roman Numerals code katas. PHP. PHPSpec

---
+++ roman_numerals/php/src/RomanNumeralsConverter.php
@@ -0,0 +1,39 @@
+<?php
+
+class RomanNumeralsConverter
+{
+    protected static $lookup = [
+        1000 => 'M',
+        900 => 'CM',
+        500 => 'D',
+        400 => 'CD',
+        100 => 'C',
+        90 => 'XC',
+        50 => 'L',
+        40 => 'XL',
+        10 => 'X',
+        9 => 'IX',
+        5 => 'V',
+        4 => 'IV',
+        1 => 'I'
+    ];
+
+    /**
+     * Convert a number into it's roman numerals form
+     * @param  int $number
+     * @return string
+     */
+    public function convert($number)
+    {
+        $solution = "";
+
+        foreach (static::$lookup as $limit => $glyph) {
+            while ($number >= $limit) {
+                $solution .= $glyph;
+                $number -= $limit;
+            }
+        }
+
+        return $solution;
+    }
+}
